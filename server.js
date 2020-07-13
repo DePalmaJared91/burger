a145f462f7dbb466fafcb7faf610b0e78ea64246
Jared DePalma <depalmajared91@gmail.com>
2020-07-12 18:21:21 -0600

    The orm.js and the seeds and schema and server are now setup

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+// var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+
 
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
 
 //serve the content stattically from public directory
-
-app.use(express.static(process.cwd() + 'public'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// app.use(express.static(process.cwd() + 'public'));
 //Override with POst ?_method=delete
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
