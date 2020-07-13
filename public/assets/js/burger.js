eeebf6a98fba254bfa93c04c4a344f5e899c8723
Jared DePalma <depalmajared91@gmail.com>
2020-07-12 21:11:23 -0600

    final push

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,26 +1,47 @@
-$(function(){
-    $(".create-form").on("submit", function(event){
+$(function () {
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
 
         var newBurger = {
             burger_name: $("#newburger")
-            .val()
-            .trim(),
+                .val()
+                .trim(),
             devoured: 0
         };
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
-        }).then(function(){
+        }).then(function () {
             console.log("Added new burger");
             location.reload();
         });
     });
-    $(".eatburger").on("click", function(event) {
+    $(".eatburger").on("click", function (event) {
         event.preventDefault();
+
         var id = $(this).data("id");
         var devouredState = {
             devoured: 1
         };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function () {
+            console.log("Burger devoured");
+            location.reload();
+        });
+    });
+
+    $(".trashburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        
+        //SEND the DELETE Request//
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(location.reload());
     });
 });
